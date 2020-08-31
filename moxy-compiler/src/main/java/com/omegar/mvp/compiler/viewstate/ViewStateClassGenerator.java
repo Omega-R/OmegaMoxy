17b8a70c92647d562b6d23a7a8f27bb445f6b577
Anton Knyazev <anton.knyazev@omega-r.com>
2020-08-31 12:12:41 +0300

    Fix generate super class

--- moxy-compiler/src/main/java/com/omegar/mvp/compiler/viewstate/ViewStateClassGenerator.java
+++ moxy-compiler/src/main/java/com/omegar/mvp/compiler/viewstate/ViewStateClassGenerator.java
@@ -75,7 +75,7 @@ public final class ViewStateClassGenerator extends JavaFilesGenerator<List<ViewI
 		}
 
 		if (filterEnd >= 0) {
-			return fileList.subList(filterEnd, fileList.size() - filterEnd);
+			return fileList.subList(filterEnd, fileList.size());
 		}
 
 		return fileList;
