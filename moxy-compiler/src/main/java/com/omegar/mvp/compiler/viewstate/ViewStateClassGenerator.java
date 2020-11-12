d62427aca0fb5e681c30870bdb6a3d05d464798a
Anton Knyazev <anton.knyazev@omega-r.com>
2020-11-12 12:25:48 +0300

    Remove NotGenerateParentClasses

--- moxy-compiler/src/main/java/com/omegar/mvp/compiler/viewstate/ViewStateClassGenerator.java
+++ moxy-compiler/src/main/java/com/omegar/mvp/compiler/viewstate/ViewStateClassGenerator.java
@@ -1,7 +1,6 @@
 package com.omegar.mvp.compiler.viewstate;
 
 import com.omegar.mvp.MvpProcessor;
-import com.omegar.mvp.NotGenerateParentClasses;
 import com.omegar.mvp.compiler.JavaFilesGenerator;
 import com.omegar.mvp.compiler.MvpCompiler;
 import com.omegar.mvp.compiler.Util;
@@ -53,18 +52,8 @@ public final class ViewStateClassGenerator extends JavaFilesGenerator<List<ViewI
 	public List<JavaFile> generate(List<ViewInterfaceInfo> list) {
 		if (list.isEmpty()) return Collections.emptyList();
 		List<JavaFile> fileList = new ArrayList<>();
-		NotGenerateParentClasses annotation = null;
-		int filterEnd = -1;
 		for (int i = 0; i < list.size(); i++) {
 			ViewInterfaceInfo info = list.get(i);
-
-			if (annotation == null) {
-				annotation = info.getElement()
-						.getAnnotation(NotGenerateParentClasses.class);
-				if (annotation != null) {
-					filterEnd = i;
-				}
-			}
 
 			JavaFile javaFile = filesMap.get(info);
 			if (javaFile == null) {
@@ -73,10 +62,6 @@ public final class ViewStateClassGenerator extends JavaFilesGenerator<List<ViewI
 			}
 
 			fileList.add(javaFile);
-		}
-
-		if (filterEnd >= 0) {
-			return fileList.subList(filterEnd, fileList.size());
 		}
 
 		return fileList;
